37386f0d8917b6012b90719372c95db0f1cbc4e1
unknown <souhail7ouabou@gmail.com>
2023-06-14 18:35:11 +0100

    add cancel reservation api

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -8,9 +8,6 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 import ListingCard from "../components/listings/ListingCard";
 
-
-
-
 interface TripsClientProps {
     reservations: Reservation[],
     currentUser?: User | null,
