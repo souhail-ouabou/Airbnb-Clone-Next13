03fcee79952ea05416aef93ed4d87a5d2f657e21
unknown <souhail7ouabou@gmail.com>
2023-06-17 02:56:45 +0100

    add properties screen

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,15 +1,41 @@
 import prisma from "@/app/libs/prismadb";
 
+export interface IListingsParams {
+    userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
+}
 
-export default async function getListing() {
+export default async function getListings(
+    params: IListingsParams
+) {
     try {
+        const {
+            userId,
+        } = params;
+
+        let query: any = {};
+
+        if (userId) {
+            query.userId = userId;
+        }
+
         const listings = await prisma.listing.findMany({
+            where: query,
             orderBy: {
                 createdAt: 'desc'
             }
-        })
+        });
+
+        ;
+
         return listings;
     } catch (error: any) {
-        throw new Error(error)
+        throw new Error(error);
     }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,21 @@ import Container from "./components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import getListing from "./actions/getListings";
 import EmptyState from "./components/EmptyState";
+import getListings, {
+  IListingsParams
+} from "@/app/actions/getListings";
 import getCurrentUser from "./actions/getCurrentUser";
 
 
-
-export default async function Home() {
-  const listings = await getListing();
+interface HomeProps {
+  searchParams: IListingsParams
+};
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListing(searchParams);
   const currentUser = await getCurrentUser();
 
- // console.log("currentUser : " ,currentUser);
-  
+  // console.log("currentUser : " ,currentUser);
+
   if (listings.length === 0) {
     return (
       <EmptyState showReset />
@@ -21,14 +26,15 @@ export default async function Home() {
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-52 xl:grid-cols-6 gap-8">
-      {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
       </div>
 
     </Container>)
 }
+export default Home;
