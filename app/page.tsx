4fc540a485526c1ae69776de7491beaa5ee4e04a
unknown <souhail7ouabou@gmail.com>
2023-06-22 06:07:57 +0100

    try to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import Container from "./components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import getListing from "./actions/getListings";
